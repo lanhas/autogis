d4021ede37130c4f8338daf739f457ea6a6f84d4
lanhas <724547369@qq.com>
2022-04-20 21:48:12 +0800

    update

---
+++ utils/metrics.py
@@ -0,0 +1,56 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+
+class MetricTracker(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+# https://stackoverflow.com/questions/48260415/pytorch-how-to-compute-iou-jaccard-index-for-semantic-segmentation
+# https://github.com/ternaus/robot-surgery-segmentation/blob/master/evaluate.py
+def jaccard_index(input, target):
+    """IoU calculation """
+    num_in_target = input.size(0)
+
+    pred = input.view(num_in_target, -1)
+    truth = target.view(num_in_target, -1)
+
+    # intersection = (pred*truth).long().sum(1).data.cpu()[0]
+    intersection = (pred * truth).sum(1)
+
+    # union = input.long().sum().data.cpu()[0] + target.long().sum().data.cpu()[0] - intersection
+    union = pred.sum(1) + truth.sum(1) - intersection
+
+    score = (intersection + 1e-15) / (union + 1e-15)
+
+    return score.mean().item()
+
+
+# https://github.com/pytorch/pytorch/issues/1249
+def dice_coeff(input, target):
+    num_in_target = input.size(0)
+
+    smooth = 1.
+
+    pred = input.view(num_in_target, -1)
+    truth = target.view(num_in_target, -1)
+
+    intersection = (pred * truth).sum(1)
+
+    loss = (2. * intersection + smooth) /(pred.sum(1) + truth.sum(1) + smooth)
+
+    return loss.mean().item()
